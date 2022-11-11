19d0ded18c438aeef461597f504b479c2e6b8055
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 17:37:32 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  return res.status(200).json({message:"Express with Netlify-Functions"})
+  return res.status(200).json({message:"YAY It WORKS!"})
 });
 
 module.exports=router
