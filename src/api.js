c14e013f13ea26959af9e56d1817907dfe31aebd
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 18:02:37 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,8 +7,13 @@ app.use(cors())
 
 
 
-app.use('/.netlify/functions/api',router)
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
-app.use("/oof", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+//app.use('/.netlify/functions/api',router)
+//app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+app.get('/oof', function (req, res) {
+  res.send('Welcome')
+})
+
+
+
 
 module.exports.handler = serverless(app);
