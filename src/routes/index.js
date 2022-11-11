70f4bc0bae9683655a95b0db7a428b958cbc12ee
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 17:48:45 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router()
 router.get("/", (req, res) => {
   return res.status(200).json({message:"YAY It WORKS!"})
 });
+router.get("/oof", (req, res) => {
+  return res.status(200).json({message:"OK Oof"})
+});
 
 module.exports=router
